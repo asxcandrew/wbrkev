bfc06680da61b5ca1f851a1c8e6877cb453f46a1
Andrey Skoryk <asxcandrew@gmail.com>
2018-10-30 18:31:05 +0200

    docker stuff

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export CGO_ENABLED=0
+export GOOS=linux
+export GOARCH=amd64
+
+dep ensure
+
+protoc -I . protos/ingestor.proto --go_out=plugins=grpc:.
+
+#go build -o build/ingestor ingestor/main.go
+#go build -o build/keeper keeper/main.go
+go build -o build/grpc grpc/main.go
